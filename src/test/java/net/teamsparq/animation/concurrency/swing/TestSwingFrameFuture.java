3fc227791d05e1e561c97e43ef427af0dcc04776
Dragos Dogaru <uk_dragos@yahoo.com>
2011-10-27 21:42:36 +0100

    one-offed properties initialization, added javadoc and logging

--- src/test/java/net/teamsparq/animation/concurrency/swing/TestSwingFrameFuture.java
+++ src/test/java/net/teamsparq/animation/concurrency/swing/TestSwingFrameFuture.java
@@ -2,7 +2,10 @@ package net.teamsparq.animation.concurrency.swing;
 
 import static junit.framework.Assert.*;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import javax.swing.*;
@@ -10,93 +13,142 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+
 public class TestSwingFrameFuture {
-  private SwingFrameFuture sff;
-  private Properties properties;
+    private SwingFrameFuture sff;
+    private static Properties properties;
+    private static Logger logger;
 
-  @Before
-  public void setUp() {
-  sff = new SwingFrameFuture();
-  properties = new Properties();
-  try {
-  properties.load(new FileInputStream("src/main/resources/properties/FutureCodeProperties"));
-      } catch (IOException ex) {
-  }
- }
+    @BeforeClass
+    public static void onlyOnce() {
+        logger = org.apache.log4j.Logger.getLogger(TestSwingFrameFuture.class);
+        BasicConfigurator.configure();
+        properties = new Properties();
+        try {
+            properties.load(new FileInputStream("src/main/resources/properties/FutureCodeProperties"));
+        } catch (IOException ex) {
+          logger.error("Properties have not been read due to an IOException. TestSwingFrameFuture failed.");
+          System.exit(0);
+        }
+    }
 
-  @Test
-  public void ensureCodeAreaDoesntOverlapPanelAndHasValidCoordinates() {
-   assertEquals(SwingFrameFuture.CODE_AREA_HEIGHT, sff.getCodeArea().getHeight());
-   assertEquals(SwingFrameFuture.CODE_AREA_WIDTH, sff.getCodeArea().getWidth());
-   assertEquals(SwingFrameFuture.CODE_AREA_X, sff.getCodeArea().getX());
-   assertEquals(SwingFrameFuture.CODE_AREA_X, sff.getCodeArea().getY());
-   assertTrue(sff.getCodeArea().getHeight() <= SwingFrameFuture.FRAME_HEIGHT);
-   assertTrue(sff.getCodeArea().getWidth() <= SwingFrameFuture.FRAME_WIDTH);
-  }
+    @Before
+    public void setUp() {
+        sff = new SwingFrameFuture();
+    }
 
-  @Test
-  public void ensureTaskSubmitterDoesntOverlapPanelAndHasValidCoordinates() {
-   assertEquals(SwingFrameFuture.TASK_SUBMITTER_HEIGHT, sff.getTaskSubmitter().getHeight());
-   assertEquals(SwingFrameFuture.TASK_SUBMITTER_WIDTH, sff.getTaskSubmitter().getWidth());
-   assertEquals(SwingFrameFuture.TASK_SUBMITTER_X, sff.getTaskSubmitter().getX());
-   assertEquals(SwingFrameFuture.TASK_SUBMITTER_Y, sff.getTaskSubmitter().getY());
-   assertTrue(sff.getTaskSubmitter().getHeight() <= SwingFrameFuture.FRAME_HEIGHT);
-   assertTrue(sff.getTaskSubmitter().getWidth() <= SwingFrameFuture.FRAME_WIDTH);
-  }
+    @Test
+    /**
+     *Testing whether the code area is correctly assigned its coordinates at initialization time and whether it's within
+     *the main frame's boundaries
+     **/
+    public void ensureCodeAreaDoesntOverlapPanelAndHasValidCoordinates() {
+        logger.info("testing ensureCodeAreaDoesntOverlapPanelAndHasValidCoordinates()...");
+        assertEquals(SwingFrameFuture.CODE_AREA_HEIGHT, sff.getCodeArea().getHeight());
+        assertEquals(SwingFrameFuture.CODE_AREA_WIDTH, sff.getCodeArea().getWidth());
+        assertEquals(SwingFrameFuture.CODE_AREA_X, sff.getCodeArea().getX());
+        assertEquals(SwingFrameFuture.CODE_AREA_X, sff.getCodeArea().getY());
+        assertTrue(sff.getCodeArea().getHeight() <= SwingFrameFuture.FRAME_HEIGHT);
+        assertTrue(sff.getCodeArea().getWidth() <= SwingFrameFuture.FRAME_WIDTH);
+    }
 
-  @Test
-  public void ensureTaskSubmitterInfoDoesntOverlapPanelAndHasValidCoordinates() {
-   assertEquals(SwingFrameFuture.TASK_SUBMITTER_INFO_HEIGHT, sff.getTaskSubmitterInfo().getHeight());
-   assertEquals(SwingFrameFuture.TASK_SUBMITTER_INFO_WIDTH, sff.getTaskSubmitterInfo().getWidth());
-   assertEquals(SwingFrameFuture.TASK_SUBMITTER_INFO_X, sff.getTaskSubmitterInfo().getX());
-   assertEquals(SwingFrameFuture.TASK_SUBMITTER_INFO_Y, sff.getTaskSubmitterInfo().getY());
-   assertTrue(sff.getTaskSubmitterInfo().getHeight() <= SwingFrameFuture.FRAME_HEIGHT);
-   assertTrue(sff.getTaskSubmitterInfo().getWidth() <= SwingFrameFuture.FRAME_WIDTH);
-  }
+    @Test
+    /**
+     *Testing whether the task submitter label is correctly assigned its coordinates at initialization time and whether
+     *it's within the main frame's boundaries
+     **/
+    public void ensureTaskSubmitterDoesntOverlapPanelAndHasValidCoordinates() {
+        logger.info("testing ensureTaskSubmitterDoesntOverlapPanelAndHasValidCoordinates()...");
+        assertEquals(SwingFrameFuture.TASK_SUBMITTER_HEIGHT, sff.getTaskSubmitter().getHeight());
+        assertEquals(SwingFrameFuture.TASK_SUBMITTER_WIDTH, sff.getTaskSubmitter().getWidth());
+        assertEquals(SwingFrameFuture.TASK_SUBMITTER_X, sff.getTaskSubmitter().getX());
+        assertEquals(SwingFrameFuture.TASK_SUBMITTER_Y, sff.getTaskSubmitter().getY());
+        assertTrue(sff.getTaskSubmitter().getHeight() <= SwingFrameFuture.FRAME_HEIGHT);
+        assertTrue(sff.getTaskSubmitter().getWidth() <= SwingFrameFuture.FRAME_WIDTH);
+    }
 
-  @Test
-  public void ensureThreadPoolDoesntOverlapPanelAndHasValidCoordinates() {
-   assertEquals(SwingFrameFuture.THREAD_POOL_HEIGHT, sff.getThreadPool().getHeight());
-   assertEquals(SwingFrameFuture.THREAD_POOL_WIDTH, sff.getThreadPool().getWidth());
-   assertEquals(SwingFrameFuture.THREAD_POOL_X, sff.getThreadPool().getX());
-   assertEquals(SwingFrameFuture.THREAD_POOL_Y, sff.getThreadPool().getY());
-   assertTrue(sff.getThreadPool().getHeight() <= SwingFrameFuture.FRAME_HEIGHT);
-   assertTrue(sff.getThreadPool().getWidth() <= SwingFrameFuture.FRAME_WIDTH);
-  }
+    @Test
+    /**
+     *Testing whether task submitter info label is correctly assigned its coordinates at initialization time and whether
+     *it's within the main frame's boundaries
+     **/
+    public void ensureTaskSubmitterInfoDoesntOverlapPanelAndHasValidCoordinates() {
+        logger.info("testing ensureTaskSubmitterInfoDoesntOverlapPanelAndHasValidCoordinates()...");
+        assertEquals(SwingFrameFuture.TASK_SUBMITTER_INFO_HEIGHT, sff.getTaskSubmitterInfo().getHeight());
+        assertEquals(SwingFrameFuture.TASK_SUBMITTER_INFO_WIDTH, sff.getTaskSubmitterInfo().getWidth());
+        assertEquals(SwingFrameFuture.TASK_SUBMITTER_INFO_X, sff.getTaskSubmitterInfo().getX());
+        assertEquals(SwingFrameFuture.TASK_SUBMITTER_INFO_Y, sff.getTaskSubmitterInfo().getY());
+        assertTrue(sff.getTaskSubmitterInfo().getHeight() <= SwingFrameFuture.FRAME_HEIGHT);
+        assertTrue(sff.getTaskSubmitterInfo().getWidth() <= SwingFrameFuture.FRAME_WIDTH);
+    }
 
-  @Test
-  public void ensureThreadPoolInfoDoesntOverlapPanelAndHasValidCoordinates() {
-   assertEquals(SwingFrameFuture.THREAD_POOL_INFO_HEIGHT, sff.getThreadPoolInfo().getHeight());
-   assertEquals(SwingFrameFuture.THREAD_POOL_INFO_WIDTH, sff.getThreadPoolInfo().getWidth());
-   assertEquals(SwingFrameFuture.THREAD_POOL_INFO_X, sff.getThreadPoolInfo().getX());
-   assertEquals(SwingFrameFuture.THREAD_POOL_INFO_Y, sff.getThreadPoolInfo().getY());
-   assertTrue(sff.getThreadPoolInfo().getHeight() <= SwingFrameFuture.FRAME_HEIGHT);
-   assertTrue(sff.getThreadPoolInfo().getWidth() <= SwingFrameFuture.FRAME_WIDTH);
-  }
+    @Test
+    /**
+     *Testing whether the thread pool is correctly assigned its coordinates at initialization time and whether it's within
+     *the main frame's boundaries
+     **/
+    public void ensureThreadPoolDoesntOverlapPanelAndHasValidCoordinates() {
+        logger.info("testing ensureThreadPoolDoesntOverlapPanelAndHasValidCoordinates()...");
+        assertEquals(SwingFrameFuture.THREAD_POOL_HEIGHT, sff.getThreadPool().getHeight());
+        assertEquals(SwingFrameFuture.THREAD_POOL_WIDTH, sff.getThreadPool().getWidth());
+        assertEquals(SwingFrameFuture.THREAD_POOL_X, sff.getThreadPool().getX());
+        assertEquals(SwingFrameFuture.THREAD_POOL_Y, sff.getThreadPool().getY());
+        assertTrue(sff.getThreadPool().getHeight() <= SwingFrameFuture.FRAME_HEIGHT);
+        assertTrue(sff.getThreadPool().getWidth() <= SwingFrameFuture.FRAME_WIDTH);
+    }
 
-  @Test
-  public void ensureResultGetterDoesntOverlapPanelAndHasValidCoordinates() {
-   assertEquals(SwingFrameFuture.RESULT_GETTER_HEIGHT, sff.getResultGetter().getHeight());
-   assertEquals(SwingFrameFuture.RESULT_GETTER_WIDTH, sff.getResultGetter().getWidth());
-   assertEquals(SwingFrameFuture.RESULT_GETTER_X, sff.getResultGetter().getX());
-   assertEquals(SwingFrameFuture.RESULT_GETTER_Y, sff.getResultGetter().getY());
-   assertTrue(sff.getResultGetter().getHeight() <= SwingFrameFuture.FRAME_HEIGHT);
-   assertTrue(sff.getResultGetter().getWidth() <= SwingFrameFuture.FRAME_WIDTH);
-  }
+    @Test
+    /**
+     *Testing whether the thread pool info label is correctly assigned its coordinates at initialization time and whether
+     *it's within the main frame's boundaries
+     **/
+    public void ensureThreadPoolInfoDoesntOverlapPanelAndHasValidCoordinates() {
+        logger.info("testing ensureThreadPoolInfoDoesntOverlapPanelAndHasValidCoordinates()...");
+        assertEquals(SwingFrameFuture.THREAD_POOL_INFO_HEIGHT, sff.getThreadPoolInfo().getHeight());
+        assertEquals(SwingFrameFuture.THREAD_POOL_INFO_WIDTH, sff.getThreadPoolInfo().getWidth());
+        assertEquals(SwingFrameFuture.THREAD_POOL_INFO_X, sff.getThreadPoolInfo().getX());
+        assertEquals(SwingFrameFuture.THREAD_POOL_INFO_Y, sff.getThreadPoolInfo().getY());
+        assertTrue(sff.getThreadPoolInfo().getHeight() <= SwingFrameFuture.FRAME_HEIGHT);
+        assertTrue(sff.getThreadPoolInfo().getWidth() <= SwingFrameFuture.FRAME_WIDTH);
+    }
 
-  @Test
-  public void verifyCodeAreaTextInConcordanceWithProperties() {
-   assertEquals(sff.getInitialCode(), properties.getProperty("initialCode"));
-   assertEquals(sff.getWaitForResultCode(), properties.getProperty("waitForResultCode"));
-   assertEquals(sff.getStartExecutionBoldCode(), properties.getProperty("startExecutionBoldCode"));
-   assertEquals(sff.getGetFutureCode(), properties.getProperty("getFutureCode"));
-  }
+    @Test
+    /**
+     *Testing whether the result getter is correctly assigned its coordinates at initialization time and whether it's
+     *within the main frame's boundaries
+     **/
+    public void ensureResultGetterDoesntOverlapPanelAndHasValidCoordinates() {
+        logger.info("testing ensureResultGetterDoesntOverlapPanelAndHasValidCoordinates()...");
+        assertEquals(SwingFrameFuture.RESULT_GETTER_HEIGHT, sff.getResultGetter().getHeight());
+        assertEquals(SwingFrameFuture.RESULT_GETTER_WIDTH, sff.getResultGetter().getWidth());
+        assertEquals(SwingFrameFuture.RESULT_GETTER_X, sff.getResultGetter().getX());
+        assertEquals(SwingFrameFuture.RESULT_GETTER_Y, sff.getResultGetter().getY());
+        assertTrue(sff.getResultGetter().getHeight() <= SwingFrameFuture.FRAME_HEIGHT);
+        assertTrue(sff.getResultGetter().getWidth() <= SwingFrameFuture.FRAME_WIDTH);
+    }
 
-  @Test
-  public void ensureValidChangesOccurWhenAnimatingLeftToRight() {
-   JComponent testLabel = new JLabel();
-   testLabel.setBounds(0, 0, 10, 10);
-   sff.animateLeftToRight(testLabel, testLabel.getX(), 30, 5);
-   assertEquals(testLabel.getX(), 30);
-  }
+    @Test
+    /**
+     *Testing whether the strings which populate the code area are correctly initialized from the properties file
+     **/
+    public void verifyCodeAreaTextInConcordanceWithProperties() {
+        logger.info("testing verifyCodeAreaTextInConcordanceWithProperties()...");
+        assertEquals(sff.getInitialCode(), properties.getProperty("initialCode"));
+        assertEquals(sff.getWaitForResultCode(), properties.getProperty("waitForResultCode"));
+        assertEquals(sff.getStartExecutionBoldCode(), properties.getProperty("startExecutionBoldCode"));
+        assertEquals(sff.getGetFutureCode(), properties.getProperty("getFutureCode"));
+    }
+
+    @Test
+    /**
+     *Testing whether the implemented animation method 'animateLeftToRight' correctly changes its argument's coordinates
+     *to simulate movement from left to right
+     **/
+    public void ensureValidChangesOccurWhenAnimatingLeftToRight() {
+        logger.info("testing ensureValidChangesOccurWhenAnimatingLeftToRight()...");
+        JComponent testLabel = new JLabel();
+        testLabel.setBounds(0, 0, 10, 10);
+        sff.animateLeftToRight(testLabel, testLabel.getX(), 30, 5);
+        assertEquals(testLabel.getX(), 30);
+    }
 }
