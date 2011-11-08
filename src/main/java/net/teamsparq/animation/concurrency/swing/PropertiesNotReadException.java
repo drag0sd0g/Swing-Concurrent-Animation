dc2dbfb8c21255a6a0ebdb9fe5c76ff08f8208fb
Dragos Dogaru <uk_dragos@yahoo.com>
2011-11-08 17:52:12 +0000

    fixed override ann

--- src/main/java/net/teamsparq/animation/concurrency/swing/PropertiesNotReadException.java
+++ src/main/java/net/teamsparq/animation/concurrency/swing/PropertiesNotReadException.java
@@ -1,6 +1,7 @@
 package net.teamsparq.animation.concurrency.swing;
 
 public class PropertiesNotReadException extends RuntimeException {
+    @Override
     public String getMessage() {
         return "Properties have not been read due to an IOException. TestSwingFrameFuture failed.";
     }
